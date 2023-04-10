8f0a6a64f83ada4e6c109687f04dd106301a7e35
UrFriendKen <51278666+UrFriendKen@users.noreply.github.com>
2023-04-10 20:16:47 +0800

    Init

---
+++ Patches/HandleNewShop_Patch.cs
@@ -0,0 +1,161 @@
+﻿using HarmonyLib;
+using Kitchen;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using UnityEngine;
+
+namespace KitchenDecorSpawnPatch.Patches
+{
+    [HarmonyPatch]
+    static class HandleNewShop_Patch
+    {
+        static readonly List<OpCode> OPCODES_TO_MATCH = new List<OpCode>()
+        {
+            OpCodes.Ldloc_S,
+            OpCodes.Ldc_I4_0,
+            OpCodes.Ceq,
+            OpCodes.Stloc_S,
+            OpCodes.Ldloc_S,
+            OpCodes.Brfalse,
+            OpCodes.Nop,
+            OpCodes.Ldarg_0,
+            OpCodes.Ldc_I4_1,
+            OpCodes.Call,
+            OpCodes.Stloc_S,
+            OpCodes.Ldc_I4_0,
+            OpCodes.Stloc_S,
+        };
+
+        static readonly List<object> OPERANDS_TO_MATCH = new List<object>()
+        {
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null
+        };
+
+        static readonly List<OpCode> MODIFIED_OPCODES = new List<OpCode>()
+        {
+            OpCodes.Nop,
+            OpCodes.Nop,
+            OpCodes.Nop,
+            OpCodes.Nop,
+            OpCodes.Nop,
+            OpCodes.Nop,
+            OpCodes.Nop,
+            OpCodes.Nop,
+            OpCodes.Nop,
+            OpCodes.Nop,
+            OpCodes.Nop,
+            OpCodes.Nop,
+            OpCodes.Nop
+        };
+
+        static readonly List<object> MODIFIED_OPERANDS = new List<object>()
+        {
+        };
+
+        const int EXPECTED_MATCH_COUNT = 1;
+
+        [HarmonyPatch(typeof(HandleNewShop), "OnUpdate")]
+        [HarmonyTranspiler]
+        static IEnumerable<CodeInstruction> OnUpdate_Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            Main.LogInfo("HandleNewShop Transpiler");
+            Main.LogInfo("Attempt remove SpawnLettersInside negation for decor application appliances");
+            List<CodeInstruction> list = instructions.ToList();
+
+            int matches = 0;
+            int windowSize = OPCODES_TO_MATCH.Count;
+            for (int i = 0; i < list.Count - windowSize; i++)
+            {
+                for (int j = 0; j < windowSize; j++)
+                {
+                    if (OPCODES_TO_MATCH[j] == null)
+                    {
+                        Main.LogError("OPCODES_TO_MATCH cannot contain null!");
+                        return instructions;
+                    }
+
+                    string logLine = $"{j}:\t{OPCODES_TO_MATCH[j]}";
+
+                    int index = i + j;
+                    OpCode opCode = list[index].opcode;
+                    if (j < OPCODES_TO_MATCH.Count && opCode != OPCODES_TO_MATCH[j])
+                    {
+                        if (j > 0)
+                        {
+                            logLine += $" != {opCode}";
+                            Main.LogInfo($"{logLine}\tFAIL");
+                        }
+                        break;
+                    }
+                    logLine += $" == {opCode}";
+
+                    if (j == 0)
+                        Debug.Log("-------------------------");
+
+                    if (j < OPERANDS_TO_MATCH.Count && OPERANDS_TO_MATCH[j] != null)
+                    {
+                        logLine += $"\t{OPERANDS_TO_MATCH[j]}";
+                        object operand = list[index].operand;
+                        if (OPERANDS_TO_MATCH[j] != operand)
+                        {
+                            logLine += $" != {operand}";
+                            Main.LogInfo($"{logLine}\tFAIL");
+                            break;
+                        }
+                        logLine += $" == {operand}";
+                    }
+                    Main.LogInfo($"{logLine}\tPASS");
+
+                    if (j == OPCODES_TO_MATCH.Count - 1)
+                    {
+                        Main.LogInfo($"Found match {++matches}");
+                        if (matches > EXPECTED_MATCH_COUNT)
+                        {
+                            Main.LogError("Number of matches found exceeded EXPECTED_MATCH_COUNT! Returning original IL.");
+                            return instructions;
+                        }
+
+                        // Perform replacements
+                        for (int k = 0; k < MODIFIED_OPCODES.Count; k++)
+                        {
+                            if (MODIFIED_OPCODES[k] != null)
+                            {
+                                int replacementIndex = i + k;
+                                OpCode beforeChange = list[replacementIndex].opcode;
+                                list[replacementIndex].opcode = MODIFIED_OPCODES[k];
+                                Main.LogInfo($"Line {replacementIndex}: Replaced Opcode ({beforeChange} ==> {MODIFIED_OPCODES[k]})");
+                            }
+                        }
+
+                        for (int k = 0; k < MODIFIED_OPERANDS.Count; k++)
+                        {
+                            if (MODIFIED_OPERANDS[k] != null)
+                            {
+                                int replacementIndex = i + k;
+                                object beforeChange = list[replacementIndex].operand;
+                                list[replacementIndex].operand = MODIFIED_OPERANDS[k];
+                                Main.LogInfo($"Line {replacementIndex}: Replaced operand ({beforeChange ?? "null"} ==> {MODIFIED_OPERANDS[k] ?? "null"})");
+                            }
+                        }
+                    }
+                }
+            }
+
+            Main.LogWarning($"{(matches > 0 ? (matches == EXPECTED_MATCH_COUNT ? "Transpiler Patch succeeded with no errors" : $"Completed with {matches}/{EXPECTED_MATCH_COUNT} found.") : "Failed to find match")}");
+            return list.AsEnumerable();
+        }
+    }
+}

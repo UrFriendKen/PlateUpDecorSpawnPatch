44e7ee7b26e84cd09fc1d3316630c14ea82e7974
UrFriendKen <51278666+UrFriendKen@users.noreply.github.com>
2023-07-01 16:47:49 +0800

    Removed KitchenLib dependency

--- Main.cs
+++ Main.cs
@@ -1,72 +1,42 @@
-﻿using KitchenLib;
-using KitchenLib.Event;
+﻿using HarmonyLib;
 using KitchenMods;
+using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
 
 // Namespace should have "Kitchen" in the beginning
 namespace KitchenDecorSpawnPatch
 {
-    public class Main : BaseMod, IModSystem
+    public class Main : IModInitializer
     {
-        // GUID must be unique and is recommended to be in reverse domain name notation
-        // Mod Name is displayed to the player and listed in the mods menu
-        // Mod Version must follow semver notation e.g. "1.2.3"
         public const string MOD_GUID = "IcedMilo.PlateUp.DecorSpawnPatch";
         public const string MOD_NAME = "Decor Spawn Patch";
-        public const string MOD_VERSION = "0.1.0";
-        public const string MOD_AUTHOR = "IcedMilo";
-        public const string MOD_GAMEVERSION = ">=1.1.5";
-        // Game version this mod is designed for in semver
-        // e.g. ">=1.1.3" current and all future
-        // e.g. ">=1.1.3 <=1.2.3" for all from/until
+        public const string MOD_VERSION = "0.1.1";
 
-        // Boolean constant whose value depends on whether you built with DEBUG or RELEASE mode, useful for testing
-#if DEBUG
-        public const bool DEBUG_MODE = true;
-#else
-        public const bool DEBUG_MODE = false;
-#endif
+        Harmony harmony;
+        List<Assembly> PatchedAssemblies = new List<Assembly>();
 
-        // public static AssetBundle Bundle;
+        public Main()
+        {
+            if (harmony == null)
+                harmony = new Harmony(MOD_GUID);
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            if (assembly != null && !PatchedAssemblies.Contains(assembly))
+            {
+                harmony.PatchAll(assembly);
+                PatchedAssemblies.Add(assembly);
+            }
+        }
 
-        public Main() : base(MOD_GUID, MOD_NAME, MOD_AUTHOR, MOD_VERSION, MOD_GAMEVERSION, Assembly.GetExecutingAssembly()) { }
-
-        protected override void OnInitialise()
+        public void PostActivate(KitchenMods.Mod mod)
         {
             LogWarning($"{MOD_GUID} v{MOD_VERSION} in use!");
         }
 
-        //private void AddGameData()
-        //{
-        //    LogInfo("Attempting to register game data...");
+        public void PreInject() { }
 
-        //    // AddGameDataObject<MyCustomGDO>();
+        public void PostInject() { }
 
-        //    LogInfo("Done loading game data.");
-        //}
-
-        protected override void OnUpdate()
-        {
-        }
-
-        protected override void OnPostActivate(KitchenMods.Mod mod)
-        {
-            // TODO: Uncomment the following if you have an asset bundle.
-            // TODO: Also, make sure to set EnableAssetBundleDeploy to 'true' in your ModName.csproj
-
-            // LogInfo("Attempting to load asset bundle...");
-            // Bundle = mod.GetPacks<AssetBundleModPack>().SelectMany(e => e.AssetBundles).First();
-            // LogInfo("Done loading asset bundle.");
-
-            // Register custom GDOs
-            // AddGameData();
-
-            // Perform actions when game data is built
-            Events.BuildGameDataEvent += delegate (object s, BuildGameDataEventArgs args)
-            {
-            };
-        }
         #region Logging
         public static void LogInfo(string _log) { Debug.Log($"[{MOD_NAME}] " + _log); }
         public static void LogWarning(string _log) { Debug.LogWarning($"[{MOD_NAME}] " + _log); }
